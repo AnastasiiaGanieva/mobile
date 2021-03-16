b204c770e98a6ef5ec3a065f037e59ff991a02cc
AnastasiiaGanieva <69317903+AnastasiiaGanieva@users.noreply.github.com>
2021-03-16 15:34:12 +0400

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main (String[] args) {
 float sum=1000f;
 int bonus =0;
-if (sum<1000){}
+if (sum<1000f){}
 else bonus= (int)sum/100;
         System.out.println("количество начисленных бонусов за пополнение счета: "+bonus);
     }
