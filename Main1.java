e964572175aec18e98540c5ce38135cf24e73db8
Anastasiya Ganieva <ganievrav@rambler.ru>
2021-03-16 13:21:44 +0400

    добавление файлов

---
+++ Main1.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main (String[] args) {
+float sum=1000f;
+int bonus =0;
+if (sum<1000){}
+else bonus= (int)sum/100;
+        System.out.println("количество начисленных бонусов за пополнение счета: "+bonus);
+    }
+
+}
